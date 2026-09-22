e5a12c9cb263767b8c632f90f9adb14b0d3a990e
Achraf <khalilou9achraf@gmail.com>
2026-09-22 23:14:41 +0000

    Add cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ import artworkRoutes from "./routes/artworkRoutes.js";
 const app = express();
 
 app.use(cors({
-  origin: "https://artwork-management-system-alpha.vercel.app"
+  origin: "https://artwork-management-system-nwrjhu354-ash-e2ec.vercel.app/"
 }));
 app.use(express.json());
 app.use(morgan("dev"));
